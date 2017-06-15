07d929b039657d7ee78c1841622ae60fef1fddc1
nriser <riserika@gmail.com>
2017-06-15 15:26:03 -0400

    Use RegEx to validate user-inputted values in edit
    Use regular expression to validate user-inputted values for
    color1 - color4 in the 'edit' view. Checks to see that the values are
    valid hexadecimal color codes.
    
    Now, if any of the colors are invalid, a flash error message will
    appear and the palette will not be created.

--- app/components/palette-edit-form.js
+++ app/components/palette-edit-form.js
@@ -1,18 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  palette: {
-    color1: null, // value gets updated by value=palette.color1 in palette-edit-form.hbs (binding the value to the model data)
-    color2: null,
-    color3: null,
-    color4: null
-  },
+  // palette: {
+  //   color1: null, // value gets updated by value=palette.color1 in palette-edit-form.hbs (binding the value to the model data)
+  //   color2: null,
+  //   color3: null,
+  //   color4: null
+  // },
   actions: {
     editPalette () {
-      console.log("hooray, inside palette-edit-form, editPalette action")
-      let newPalette = this.get('palette'); // this gets 'palette' from line 4, but one with data that user inputted.
-      console.log('newPalette in palette-edit.js', newPalette);
-      this.sendAction('editPalette', newPalette);
+      console.log("hooray, inside palette-edit-form, editPalette action");
+      let editedPalette = this.get('palette'); // this gets 'palette' from line 4, but one with data that user inputted. At this point, this.get('palette') already has been updated with edited palette colors, and is changes its value anytime it is changed.
+      console.log('editedPalette in palette-edit.js', editedPalette);
+      console.log('what is edited.color1 in palette-edit-form.js', editedPalette.color1);
+      console.log('what is edited.color1 in palette-edit-form.js', editedPalette.get('color1'));
+
+      if (editedPalette.get('color1').match(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/) && editedPalette.get('color2').match(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/) &&
+      editedPalette.get('color3').match(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/) &&
+      editedPalette.get('color4').match(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/)) {
+        this.sendAction('editPalette', editedPalette);
+      } else {
+        this.get('flashMessages')
+        .danger("Error: Please provide a valid hex code, starting with a '#'");
+      }
+
+
+      // this.sendAction('editPalette', newPalette);
     },
     cancel () {
       console.log('at cancel in components/palette-edit-form.js');
