4cb0ff71c196c6bbcd14aadfdd3a6fd7ab442781
nriser <riserika@gmail.com>
2017-06-26 18:50:08 -0400

    Add bootstrap font files to use glyphicons
    Add bootstrap font files in public/assets/ folder. Created an 'assets'
    folder in order for Ember to recognize the font files.
    
    Use glyphicons in Sign Up, Sign In, Change Password, My Palettes, and
    Create Palette pages.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,9 +1,9 @@
 /*jshint node:true*/
 /* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
+  const app = new EmberApp(defaults, {
     sassOptions: {
       includePaths: [
         'bower_components/bootstrap-sass/assets/stylesheets'
@@ -12,6 +12,10 @@ module.exports = function(defaults) {
   });
 
   app.import('bower_components/bootstrap-sass/assets/javascripts/bootstrap.js');
+
+  app.import('bower_components/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.woff', {
+    destDir: 'fonts/bootstrap/'
+  });
 
   return app.toTree();
 };
